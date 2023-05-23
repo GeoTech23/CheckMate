721ebc088569c1fdcb6902cf7d42f641f22e28ea
atvanek <99850013+atvanek@users.noreply.github.com>
2023-05-23 12:47:08 -0400

    Changed box shadow on contact div

--- src/components/styled/ContactDiv.tsx
+++ src/components/styled/ContactDiv.tsx
@@ -7,7 +7,7 @@ const ContactDiv = styled.div`
 	height: 100px;
 	width: 100px;
 	justify-content: center;
-	box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+	box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
 `;
 
 export default ContactDiv;
