da09f7912d958ca47276aefdf12819a0fb18b53a
wiris316 <wiris316@gmail.com>
2023-05-24 13:53:56 -0400

    Twilio API
    Co-authored-by: Adam Vanek <atvanek@users.noreply.github.com> Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com> Co-authored-by: Elena Atencio <elenaatencio@users.noreply.github.com>

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,10 +4,88 @@ import chatRouter from './routes/chatRouter.ts';
 import contactRouter from './routes/contactRouter.ts';
 import authRouter from './routes/authRouter.ts'
 import authController from './controllers/authController.ts'
+import Twilio from 'twilio'
+import { dirname } from 'path';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-// import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenv.config({path: path.resolve(dirname('./.env'))})
+
 const app = express();
 const PORT = 3000;
+
+
+const envPath = path.resolve(__dirname, '.env');
+dotenv.config({ path: envPath });
+
+
+
+
+// Your Account SID, Subaccount SID Auth Token from console.twilio.com
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN 
+
+console.log('data here', new Date())
+
+console.log('accountSId', accountSid)
+
+const client = Twilio(accountSid, authToken);
+const numToMsg = process.env.NUMS
+const twilioNum = process.env.TWILIO_NUM
+
+function sendMessage() {
+
+  numToMsg.forEach(function(num){
+    let message = client.messages.create({
+      body: 'Time to call mom! ENJOY LUNCH ADAM',
+      to:  num,
+      from: twilioNum, // From a valid Twilio number
+      // sendAt: new Date('2023-05-24T15:43:01+0000'),
+      // scheduleType: 'fixed',
+    })
+    .then(message => console.log(message.status))
+  })
+}
+    // .create({
+
+    // })
+//     .then((message) => {
+//       console.log('Message sent successfully with SID:', message.sid);
+//     })
+//     .catch((error) => {
+//       console.error('Error sending message:', error);
+//     });
+// }
+
+function scheduleMessage() {
+  const now: Date = new Date();
+  const nextSendTime: Date = new Date(now.getTime() + 1 * 60 * 1000); // Set the next send time 2 minutes later
+  const timeOut: number = nextSendTime.getTime() - now.getTime();
+
+  setTimeout(async () => {
+    await sendMessage();
+    scheduleMessage(); 
+  }, timeOut);
+}
+
+// scheduleMessage(); 
+
+
+
+// const client = Twilio(accountSid, authToken);
+// client.messages
+//   .create({
+//     body: 'newMessage',
+//     to: '+16465525220', // Text your number
+//     from: '+18665615608', // From a valid Twilio number
+//     sendAt: new Date('2023-05-24T15:43:01+0000'),
+//     scheduleType: 'fixed',
+//   })
+//   .then((message) => console.log(message.sid));
+  
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
@@ -17,11 +95,11 @@ app.use('/contact', contactRouter);
 
 app.post('/signup', authController.createUser, (req: Request, res: Response) => {
 	if (res.locals.user === true) {
-	  console.log(res.locals.user)
-	  return res.status(200).json(res.locals.user)
+    console.log(res.locals.user)
+    return res.status(200).json(res.locals.user)
 	} else {
-	  console.log(false)
-	  throw new Error
+    console.log(false)
+    throw new Error
 	}
 })
 
@@ -29,7 +107,8 @@ app.post('/signup', authController.createUser, (req: Request, res: Response) => 
 
 //catch-all router handler
 app.use('/', (_req: Request, res: Response) => {
-	console.log('Page not found')
+	console.log('In catch-all route')
+  res.status(404).send('Page Not Found')
 });
 
 //Global error handler
